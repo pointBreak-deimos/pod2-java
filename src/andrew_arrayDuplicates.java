73b2d438b86c4d7222f7491c8a70aa7bf24af61c
andrewswint <andrewswint1976@gmail.com>
2019-12-03 13:53:53 -0600

    2 of three complete

---
+++ src/andrew_arrayDuplicates.java
@@ -0,0 +1,16 @@
+public class andrew_arrayDuplicates {
+
+    public static void main(String[] args) {
+
+        String[] array = {"one", "two", "three", "four", "one", "five", "three"};
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int x = i + 1; x < array.length; x++) {
+                if (array[i].equals(array[x])) {
+                    System.out.println(array[x] + " is a duplicate");
+                }
+            }
+        }
+
+    }
+}
